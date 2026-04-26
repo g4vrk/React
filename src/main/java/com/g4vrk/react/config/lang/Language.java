e9ea9de361c986cb75da0f7430728522ec20cdda
g4vrk <snegrik123@gmail.com>
2026-04-26 22:46:20 +0300

    refactor: remove logging

--- src/main/java/com/g4vrk/react/config/lang/Language.java
+++ src/main/java/com/g4vrk/react/config/lang/Language.java
@@ -1,14 +1,10 @@
 package com.g4vrk.react.config.lang;
 
-import com.g4vrk.react.log.FastLog;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
 
 public enum Language {
     EN,
     RU;
-
-    private static final Logger LOGGER = FastLog.newLogger("Language");
 
     public static @NotNull Language resolve() {
         try {
