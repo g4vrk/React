f3fa7d26cf7800aa90fcdbac1c5f2ae2ad5a6b40
g4vrk <snegrik123@gmail.com>
2026-04-26 22:50:01 +0300

    feat: config manager, config values

--- src/main/java/com/g4vrk/react/React.java
+++ src/main/java/com/g4vrk/react/React.java
@@ -1,5 +1,8 @@
 package com.g4vrk.react;
 
+import com.g4vrk.react.config.YamlConfig;
+import com.g4vrk.react.config.lang.Language;
+import com.g4vrk.react.config.manager.YamlConfigManager;
 import com.g4vrk.react.config.values.ConfigValues;
 import com.g4vrk.react.ml.server.MLServer;
 import com.g4vrk.react.runner.factory.TaskRunnerFactory;
@@ -18,14 +21,23 @@ import org.slf4j.LoggerFactory;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class React {
 
-    public static final String NAME = "React";
     public static final React INSTANCE = new React();
+
+    public static final String NAME = "React";
     public static final Logger LOGGER = LoggerFactory.getLogger(NAME);
+
+    private static final String MAIN_CONFIG_NAME = "main-config";
 
     private Plugin plugin;
 
+    private YamlConfigManager yamlConfigManager;
+
+    private YamlConfig mainConfig;
+
     private ConfigValues configValues;
+
     private TaskRunnerFactory taskRunnerFactory;
+
     private MLServer mlServer;
 
     void initialize(final @NotNull Plugin plugin) {
@@ -33,13 +45,21 @@ public class React {
     }
 
     void load() {
+        this.yamlConfigManager = new YamlConfigManager(plugin, Language.resolve());
+        this.mainConfig = yamlConfigManager.getConfig(MAIN_CONFIG_NAME);
+
+        this.configValues = new ConfigValues(mainConfig);
+
         this.taskRunnerFactory = ServerVersion.isFolia()
                 ? new FoliaTaskRunnerFactory(plugin)
                 : new PaperTaskRunnerFactory(plugin);
+
         this.mlServer = new MLServer(configValues.getMlClientSettings());
     }
 
     void terminate() {
+        this.mainConfig = null;
+        this.configValues = null;
         this.taskRunnerFactory = null;
         this.mlServer = null;
     }
