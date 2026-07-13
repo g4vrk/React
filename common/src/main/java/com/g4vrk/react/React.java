5865f2928ca015fbc9e8564d80bed73ff0f8a70b
g4vrk <snegrik123@gmail.com>
2026-07-14 00:22:53 +0300

    feat: normal logs & textFormatter instead of LCS

--- common/src/main/java/com/g4vrk/react/React.java
+++ common/src/main/java/com/g4vrk/react/React.java
@@ -1,5 +1,6 @@
 package com.g4vrk.react;
 
+import com.g4vrk.fastTextFormatter.TextFormatter;
 import com.g4vrk.functionalConfiguration.Config;
 import com.g4vrk.react.alert.manager.AlertManager;
 import com.g4vrk.react.alert.printer.AlertPrinter;
@@ -22,23 +23,25 @@ import com.g4vrk.react.resource.ResourceHolder;
 import com.g4vrk.react.resource.impl.PluginResourceHolder;
 import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.event.EventManager;
+import com.github.retrooper.packetevents.event.PacketListenerCommon;
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.jetbrains.annotations.NotNull;
-import org.jspecify.annotations.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 import java.util.concurrent.ForkJoinPool;
 
 @Getter
@@ -49,7 +52,11 @@ public class React {
 
     private static final String NAME = "React";
 
+    private static final String[] SPECIAL_THANKS_TO = new String[]{"jvm-argument", "g4vrk"};
+
     private final Logger logger = LoggerFactory.getLogger(NAME);
+
+    private final TextFormatter textFormatter = TextFormatter.textFormatter();
 
     private Plugin plugin;
 
@@ -70,6 +77,8 @@ public class React {
 
     private AlertPublisher alertPublisher;
     private AlertManager alertManager;
+
+    private final Set<PacketListenerCommon> registeredListeners = new ObjectOpenHashSet<>();
 
     public static @NotNull React getSingletonInstance() {
         return INSTANCE;
@@ -103,9 +112,11 @@ public class React {
 
         this.configMap = new Object2ObjectOpenHashMap<>();
         try {
+
             this.yamlConfigManager.prepareExpected(resourceHolder, languageName, configsDir);
 
             this.configMap.putAll(yamlConfigManager.loadAndSave(resourceHolder, configsDir));
+
         } catch (final Exception ex) {
             throw new RuntimeException("An internal error occurred when trying to load configurations", ex);
         }
@@ -125,15 +136,20 @@ public class React {
         final String alertFormatRaw = mainConfig.getRoot()
                 .node("alerts", "format")
                 .getString("&c«React» &7| &f{player} - {check} {verbose}");
-        final Component alertFormat = LegacyComponentSerializer.legacyAmpersand().deserialize(alertFormatRaw);
+
+        final Component alertFormat = textFormatter.format(alertFormatRaw);
 
         final MLCheck mlCheck = createMlCheck(taskRunner, alertFormat);
 
         final EventManager eventManager = PacketEvents.getAPI().getEventManager();
 
+        final ConnectionListener connectionListener = new ConnectionListener(playerRegistry, alertPublisher, configValues.getBufferSize());
+
+
         eventManager.registerListener(
-                new ConnectionListener(playerRegistry, alertPublisher, configValues.getBufferSize())
+                connectionListener
         );
+
         eventManager.registerListener(
                 new RotationListener(playerRegistry, mlCheck)
         );
@@ -148,10 +164,54 @@ public class React {
                 plugin
         );
 
-        logger.info("React успешно включен.");
+        logger.info(" ");
+        logger.info("React successfully enabled!");
+        logger.info(" ");
+        logger.info("Thanks for using our AI powered products.");
+        logger.info("Special thanks to {}", String.join(", ", SPECIAL_THANKS_TO));
+        logger.info(" ");
+        logger.info("Our telegram channel: https://telegram.me/react_ac");
+        logger.info(" ");
     }
 
-    private @NonNull MLCheck createMlCheck(@NotNull TaskRunner taskRunner, @NotNull Component alertFormat) {
+    public void terminate() {
+
+        try {
+
+            this.unregisterPacketListeners();
+
+            this.mainConfig = null;
+            this.configValues = null;
+            this.taskRunnerFactory = null;
+            this.mlServer = null;
+
+        } catch (final Exception ex) {
+            this.logger.error("An internal error occurred when trying to terminate the plugin", ex);
+        }
+
+    }
+
+    private void registerPacketListeners(
+            final @NotNull PacketListenerCommon @NotNull ... packetListeners
+    ) {
+
+        final EventManager eventManager = PacketEvents.getAPI().getEventManager();
+
+        eventManager.registerListeners(packetListeners);
+
+        this.registeredListeners.addAll(new ObjectArrayList<>(packetListeners));
+
+    }
+
+    private void unregisterPacketListeners() {
+
+        final EventManager eventManager = PacketEvents.getAPI().getEventManager();
+
+        eventManager.unregisterListeners(this.registeredListeners.toArray(PacketListenerCommon[]::new));
+
+    }
+
+    private @NotNull MLCheck createMlCheck(@NotNull TaskRunner taskRunner, @NotNull Component alertFormat) {
         this.alertManager = new AlertManager();
 
         this.alertPublisher = new AlertPublisher(
@@ -173,13 +233,6 @@ public class React {
         return new MLCheck(
                 taskRunner, mlCheckProcessor, 10
         );
-    }
-
-    public void terminate() {
-        this.mainConfig = null;
-        this.configValues = null;
-        this.taskRunnerFactory = null;
-        this.mlServer = null;
     }
 
 }
