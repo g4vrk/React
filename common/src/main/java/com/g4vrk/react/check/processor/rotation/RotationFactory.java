832f9b809ab7c68abe979129376af53f152edeae
g4vrk <snegrik123@gmail.com>
2026-07-14 23:54:03 +0300

    fix: fixed all data fully... (STUPID MISTAKE fix...)

--- common/src/main/java/com/g4vrk/react/check/processor/rotation/RotationFactory.java
+++ common/src/main/java/com/g4vrk/react/check/processor/rotation/RotationFactory.java
@@ -16,22 +16,21 @@ public final class RotationFactory {
         final float normalizedYaw = AngleMath.normalizeAngle(yaw);
         final float normalizedPitch = AngleMath.normalizeAngle(pitch);
 
-        final float deltaYaw = previous == null ? 0F : normalizedYaw - previous.getPitch();
-        final float deltaPitch = previous == null ? 0F : normalizedPitch - previous.getYaw();
+        final float deltaYaw = previous == null ? 0F : normalizedYaw - previous.getYaw();
+        final float deltaPitch = previous == null ? 0F : normalizedPitch - previous.getPitch();
 
-        final float jerkYaw = previous == null ? 0F : AngleMath.calculateJerk(deltaYaw, previous.getDeltaPitch());
-
-        final float jerkPitch = previous == null ? 0F : AngleMath.calculateJerk(deltaPitch, previous.getDeltaYaw());
+        final float jerkYaw = previous == null ? 0F : AngleMath.calculateJerk(deltaYaw, previous.getDeltaYaw());
+        final float jerkPitch = previous == null ? 0F : AngleMath.calculateJerk(deltaPitch, previous.getDeltaPitch());
 
         return new Rotation(
-                normalizedYaw,
                 normalizedPitch,
-                deltaYaw,
+                normalizedYaw,
                 deltaPitch,
-                jerkYaw,
+                deltaYaw,
                 jerkPitch,
-                AngleMath.calculateGCDError(deltaYaw),
-                AngleMath.calculateGCDError(deltaPitch)
+                jerkYaw,
+                AngleMath.calculateGCDError(deltaPitch),
+                AngleMath.calculateGCDError(deltaYaw)
         );
 
     }
