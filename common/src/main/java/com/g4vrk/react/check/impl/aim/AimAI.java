ea91aadfdc0ef6adf760b3d5bb57e07c31848e85
g4vrk <snegrik123@gmail.com>
2026-07-14 18:20:03 +0300

    feat: add combat activity check

--- common/src/main/java/com/g4vrk/react/check/impl/aim/AimAI.java
+++ common/src/main/java/com/g4vrk/react/check/impl/aim/AimAI.java
@@ -54,7 +54,7 @@ public final class AimAI extends Check implements RotationCheck {
 
     @Override
     public void onRotation(@NotNull RotationData currentData) {
-        if (currentData.historySize() < this.requiredSamples) return;
+        if (!player.combatActivity.isActive() || currentData.historySize() < this.requiredSamples) return;
 
         final Rotation[] snapshot = currentData.snapshotHistory();
 
