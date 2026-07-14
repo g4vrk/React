0197eddc19c00723cdb5b145f00cc42ba45f7491
g4vrk <snegrik123@gmail.com>
2026-07-14 14:09:31 +0300

    feat: PlayerFactory

---
+++ common/src/main/java/com/g4vrk/react/player/factory/PlayerFactory.java
@@ -0,0 +1,21 @@
+package com.g4vrk.react.player.factory;
+
+import com.g4vrk.react.player.model.ReactPlayer;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+public final class PlayerFactory {
+
+    private final int rotationsBufferSize;
+
+    public @NotNull ReactPlayer create(
+            final @NotNull UUID uniqueId,
+            final @NotNull String name
+    ) {
+        return new ReactPlayer(uniqueId, name, rotationsBufferSize);
+    }
+
+}
