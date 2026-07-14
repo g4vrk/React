c55d866127fdd1a6277bb2d5283655f6fd50522f
g4vrk <snegrik123@gmail.com>
2026-07-14 14:08:08 +0300

    feat: RotationProcessor

---
+++ common/src/main/java/com/g4vrk/react/check/processor/rotation/RotationProcessor.java
@@ -0,0 +1,32 @@
+package com.g4vrk.react.check.processor.rotation;
+
+import com.g4vrk.react.game.Rotation;
+import com.g4vrk.react.player.model.ReactPlayer;
+import com.g4vrk.react.player.model.rotation.RotationData;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+@RequiredArgsConstructor
+public final class RotationProcessor {
+
+    private final RotationFactory rotationFactory;
+
+    public void process(
+            final @NotNull ReactPlayer player,
+            final float yaw,
+            final float pitch
+    ) {
+
+        final RotationData rotationData = player.rotationData;
+
+        final Rotation previous = rotationData.current();
+
+        final Rotation current = this.rotationFactory.create(yaw, pitch, previous);
+
+        rotationData.push(current);
+
+        player.checkManager.onRotationChange(rotationData);
+
+    }
+
+}
