733d2c2ffab2137945699168e6fc7aa07d1a99bb
g4vrk <snegrik123@gmail.com>
2026-07-25 16:59:52 +0300

    fix(permissions): make constants public

--- common/src/main/java/com/g4vrk/react/Permissions.java
+++ common/src/main/java/com/g4vrk/react/Permissions.java
@@ -10,15 +10,15 @@ import org.jetbrains.annotations.NotNull;
 @FieldDefaults(makeFinal = true, level = AccessLevel.PUBLIC)
 public class Permissions {
 
-    @NotNull String PERMISSION_PREFIX = "react.";
+    public @NotNull String PERMISSION_PREFIX = "react.";
 
-    @NotNull String ALERTS = PERMISSION_PREFIX + "alerts";
-    @NotNull String ALERTS_ENABLE_ON_JOIN = ALERTS + ".enable_on_join";
+    public @NotNull String ALERTS = PERMISSION_PREFIX + "alerts";
+    public @NotNull String ALERTS_ENABLE_ON_JOIN = ALERTS + ".enable_on_join";
 
-    @NotNull String VERBOSE = PERMISSION_PREFIX + "verbose";
-    @NotNull String VERBOSE_ENABLE_ON_JOIN = VERBOSE + ".enable_on_join";
-    
-    @NotNull String BASE_BYPASS = PERMISSION_PREFIX + "bypass";
+    public @NotNull String VERBOSE = PERMISSION_PREFIX + "verbose";
+    public @NotNull String VERBOSE_ENABLE_ON_JOIN = VERBOSE + ".enable_on_join";
+
+    public @NotNull String BASE_BYPASS = PERMISSION_PREFIX + "bypass";
 
     public boolean hasBypassForCheck(
             final @NotNull Permissible permissible,
