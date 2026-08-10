ee5794444b5b7e205a2810de29fe9cce0f84521d
g4vrk <snegrik123@gmail.com>
2026-08-10 20:20:39 +0300

    refactor(history): normalize pagination

--- common/src/main/java/com/g4vrk/react/history/printer/InferenceHistoryPrinter.java
+++ common/src/main/java/com/g4vrk/react/history/printer/InferenceHistoryPrinter.java
@@ -1,6 +1,7 @@
 package com.g4vrk.react.history.printer;
 
 import com.g4vrk.fastTextFormatter.TextFormatter;
+import com.g4vrk.fastTextFormatter.function.TextPreProcessor;
 import com.g4vrk.functionalConfiguration.Config;
 import com.g4vrk.react.React;
 import com.g4vrk.react.api.ReloadObserver;
@@ -25,10 +26,10 @@ public class InferenceHistoryPrinter implements ReloadObserver {
     private final ValueColorResolver confidenceColorResolver;
     private final ValueColorResolver probabilityColorResolver;
 
-    private Component header;
-    private Component entryFormat;
-    private Component empty;
-    private Component footer;
+    private String header;
+    private String entryFormat;
+    private String empty;
+    private String footer;
 
     private int printEntries;
 
@@ -57,65 +58,69 @@ public class InferenceHistoryPrinter implements ReloadObserver {
 
         try {
 
-            this.header = formatList(
-                    config.node(
-                            "history",
-                            "inference",
-                            "format",
-                            "header"
-                    ).getList(String.class, Collections.emptyList())
+            this.header = loadFormat(
+                    config,
+                    "header"
             );
 
-            this.entryFormat = formatList(
-                    config.node(
-                            "history",
-                            "inference",
-                            "format",
-                            "entry-format"
-                    ).getList(String.class, Collections.emptyList())
+            this.entryFormat = loadFormat(
+                    config,
+                    "entry-format"
             );
 
-            this.empty = formatList(
-                    config.node(
-                            "history",
-                            "inference",
-                            "format",
-                            "empty"
-                    ).getList(String.class, Collections.emptyList())
+            this.empty = loadFormat(
+                    config,
+                    "empty"
             );
 
-            this.footer = formatList(
-                    config.node(
-                            "history",
-                            "inference",
-                            "format",
-                            "footer"
-                    ).getList(String.class, Collections.emptyList())
+            this.footer = loadFormat(
+                    config,
+                    "footer"
             );
 
         } catch (final SerializationException ex) {
             throw new RuntimeException(ex);
         }
 
-        this.printEntries = config.node(
-                "history",
-                "inference",
-                "format",
-                "print-entries"
-        ).getInt(18);
+        this.printEntries = Math.max(
+                1,
+                config.node(
+                        "history",
+                        "inference",
+                        "format",
+                        "print-entries"
+                ).getInt(18)
+        );
     }
 
-    private @NotNull Component formatList(
-            final @NotNull List<String> lines
-    ) {
-        return textFormatter.format(String.join("<newline>", lines));
+    private @NotNull String loadFormat(
+            final @NotNull Config config,
+            final @NotNull String key
+    ) throws SerializationException {
+
+        return String.join(
+                "<newline>",
+                config.node(
+                        "history",
+                        "inference",
+                        "format",
+                        key
+                ).getList(String.class, Collections.emptyList())
+        );
+
     }
 
     public void print(
             final @NotNull Audience receiver,
             final @NotNull ReactPlayer player
     ) {
-        print(receiver, player, 1);
+
+        print(
+                receiver,
+                player,
+                1
+        );
+
     }
 
     public void print(
@@ -123,44 +128,69 @@ public class InferenceHistoryPrinter implements ReloadObserver {
             final @NotNull ReactPlayer player,
             int page
     ) {
+
         final InferenceHistoryEntry[] history =
                 player.inferenceHistory.entries();
 
         final int totalPages = Math.max(
                 1,
-                (int) Math.ceil((double) history.length / this.printEntries)
+                (int) Math.ceil(
+                        (double) history.length / this.printEntries
+                )
         );
 
-        page = Math.max(1, Math.min(page, totalPages));
-
-        receiver.sendMessage(
-                replaceCommon(
-                        this.header,
-                        player,
-                        history,
+        page = Math.max(
+                1,
+                Math.min(
                         page,
                         totalPages
                 )
         );
 
+        final InferenceStatistic.Result statisticResult =
+                player.inferenceStatistic.calculate();
+
+        final double avgProbability =
+                statisticResult.averageProbability();
+
+        final double avgConfidence =
+                statisticResult.averageConfidence();
+
+        final TextPreProcessor commonProcessor = createCommonProcessor(
+                player,
+                history,
+                page,
+                totalPages,
+                avgProbability,
+                avgConfidence
+        );
+
+        receiver.sendMessage(
+                formatCommon(
+                        this.header,
+                        commonProcessor,
+                        avgProbability,
+                        avgConfidence
+                )
+        );
+
         if (history.length == 0) {
+
             receiver.sendMessage(
-                    replaceCommon(
+                    formatCommon(
                             this.empty,
-                            player,
-                            history,
-                            page,
-                            totalPages
+                            commonProcessor,
+                            avgProbability,
+                            avgConfidence
                     )
             );
 
             receiver.sendMessage(
-                    replaceCommon(
+                    formatCommon(
                             this.footer,
-                            player,
-                            history,
-                            page,
-                            totalPages
+                            commonProcessor,
+                            avgProbability,
+                            avgConfidence
                     )
             );
 
@@ -168,7 +198,8 @@ public class InferenceHistoryPrinter implements ReloadObserver {
         }
 
         final int start =
-                history.length - 1 - ((page - 1) * this.printEntries);
+                history.length - 1
+                        - ((page - 1) * this.printEntries);
 
         final int end = Math.max(
                 -1,
@@ -176,147 +207,200 @@ public class InferenceHistoryPrinter implements ReloadObserver {
         );
 
         for (int i = start; i > end; i--) {
-            Component component = replaceCommon(
-                    this.entryFormat,
-                    player,
-                    history,
-                    page,
-                    totalPages
+
+            receiver.sendMessage(
+                    formatEntry(
+                            this.entryFormat,
+                            commonProcessor,
+                            history[i],
+                            avgProbability,
+                            avgConfidence
+                    )
             );
 
-            component = replaceEntry(
-                    component,
-                    history[i]
-            );
-
-            receiver.sendMessage(component);
         }
 
         receiver.sendMessage(
-                replaceCommon(
+                formatCommon(
                         this.footer,
-                        player,
-                        history,
-                        page,
-                        totalPages
+                        commonProcessor,
+                        avgProbability,
+                        avgConfidence
                 )
         );
     }
 
-    private @NotNull Component replaceCommon(
-            @NotNull Component component,
-            @NotNull ReactPlayer player,
-            @NotNull InferenceHistoryEntry[] history,
-            int currentPage,
-            int maxPage
+    private @NotNull TextPreProcessor createCommonProcessor(
+            final @NotNull ReactPlayer player,
+            final @NotNull InferenceHistoryEntry[] history,
+            final int currentPage,
+            final int maxPage,
+            final double avgProbability,
+            final double avgConfidence
     ) {
-        final InferenceStatistic.Result statisticResult = player.inferenceStatistic.calculate();
 
-        final double avgProbability = statisticResult.averageProbability();
-        final double avgConfidence = statisticResult.averageConfidence();
+        return text -> text
+                .replace(
+                        "{player}",
+                        player.getName()
+                )
+                .replace(
+                        "{size}",
+                        String.valueOf(history.length)
+                )
+                .replace(
+                        "{page:previous}",
+                        String.valueOf(
+                                Math.max(
+                                        1,
+                                        currentPage - 1
+                                )
+                        )
+                )
+                .replace(
+                        "{page:current}",
+                        String.valueOf(currentPage)
+                )
+                .replace(
+                        "{page:next}",
+                        String.valueOf(
+                                Math.min(
+                                        maxPage,
+                                        currentPage + 1
+                                )
+                        )
+                )
+                .replace(
+                        "{page:max}",
+                        String.valueOf(maxPage)
+                )
+                .replace(
+                        "{avg-probability}",
+                        String.valueOf(avgProbability)
+                )
+                .replace(
+                        "{avg-confidence}",
+                        String.valueOf(avgConfidence)
+                );
 
-        component = replace(
-                component,
-                "{player}",
-                Component.text(player.getName())
+    }
+
+    private @NotNull Component formatCommon(
+            final @NotNull String input,
+            final @NotNull TextPreProcessor commonProcessor,
+            final double avgProbability,
+            final double avgConfidence
+    ) {
+
+        Component component = this.textFormatter.formatWithPreProcessors(
+                input,
+                List.of(
+                        commonProcessor
+                )
         );
 
-        component = replace(
+        component = replaceColoredCommon(
                 component,
-                "{size}",
-                Component.text(history.length)
-        );
-
-        component = replace(
-                component,
-                "{page:previous}",
-                Component.text(Math.max(1, currentPage - 1))
-        );
-
-        component = replace(
-                component,
-                "{page:current}",
-                Component.text(currentPage)
-        );
-
-        component = replace(
-                component,
-                "{page:next}",
-                Component.text(Math.min(maxPage, currentPage + 1))
-        );
-
-        component = replace(
-                component,
-                "{page:max}",
-                Component.text(maxPage)
-        );
-
-        component = replace(
-                component,
-                "{avg-probability}",
-                Component.text(avgProbability)
-        );
-
-        component = replace(
-                component,
-                "{avg-probability:colored}",
-                Component.text(avgProbability)
-                        .color(probabilityColorResolver.resolve(avgProbability))
-        );
-
-        component = replace(
-                component,
-                "{avg-confidence}",
-                Component.text(avgConfidence)
-        );
-
-        component = replace(
-                component,
-                "{avg-confidence:colored}",
-                Component.text(avgConfidence)
-                        .color(confidenceColorResolver.resolve(avgConfidence))
+                avgProbability,
+                avgConfidence
         );
 
         return component;
     }
 
-    private @NotNull Component replaceEntry(
-            @NotNull Component component,
-            @NotNull InferenceHistoryEntry entry
+    private @NotNull Component formatEntry(
+            final @NotNull String input,
+            final @NotNull TextPreProcessor commonProcessor,
+            final @NotNull InferenceHistoryEntry entry,
+            final double avgProbability,
+            final double avgConfidence
     ) {
-        final double probability = entry.getProbability();
-        final double confidence = entry.getConfidence();
 
-        component = replace(
+        final double probability =
+                entry.getProbability();
+
+        final double confidence =
+                entry.getConfidence();
+
+        final TextPreProcessor entryProcessor = text -> text
+                .replace(
+                        "{probability}",
+                        String.valueOf(probability)
+                )
+                .replace(
+                        "{confidence}",
+                        String.valueOf(confidence)
+                )
+                .replace(
+                        "{check}",
+                        entry.getCheck().getName()
+                );
+
+        Component component = this.textFormatter.formatWithPreProcessors(
+                input,
+                List.of(
+                        commonProcessor,
+                        entryProcessor
+                )
+        );
+
+        component = replaceColoredCommon(
                 component,
-                "{probability}",
-                Component.text(probability)
+                avgProbability,
+                avgConfidence
         );
 
         component = replace(
                 component,
                 "{probability:colored}",
                 Component.text(probability)
-                        .color(probabilityColorResolver.resolve(probability))
-        );
-
-        component = replace(
-                component,
-                "{confidence}",
-                Component.text(confidence)
+                        .color(
+                                this.probabilityColorResolver.resolve(
+                                        probability
+                                )
+                        )
         );
 
         component = replace(
                 component,
                 "{confidence:colored}",
                 Component.text(confidence)
-                        .color(confidenceColorResolver.resolve(confidence))
+                        .color(
+                                this.confidenceColorResolver.resolve(
+                                        confidence
+                                )
+                        )
+        );
+
+        return component;
+    }
+
+    private @NotNull Component replaceColoredCommon(
+            @NotNull Component component,
+            final double avgProbability,
+            final double avgConfidence
+    ) {
+
+        component = replace(
+                component,
+                "{avg-probability:colored}",
+                Component.text(avgProbability)
+                        .color(
+                                this.probabilityColorResolver.resolve(
+                                        avgProbability
+                                )
+                        )
         );
 
         component = replace(
                 component,
-                "{check}",
-                Component.text(entry.getCheck().getName())
+                "{avg-confidence:colored}",
+                Component.text(avgConfidence)
+                        .color(
+                                this.confidenceColorResolver.resolve(
+                                        avgConfidence
+                                )
+                        )
         );
 
         return component;
